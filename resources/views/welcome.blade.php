a664e4531491b4bc94cf260a20382f7d16f2e0a0
Trinidad, Mark Aris M <mark.aris.m.trinidad@accenture.com>
2015-09-23 18:33:55 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" >
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>           
+        </div>      
+        <script type="text/javascript" src="{{ URL::asset('js/angular.min.js') }}"></script>
+    </body>
+</html>
